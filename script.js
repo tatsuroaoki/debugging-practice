a150735e183093a840ef6603110d52694b25cb1a
tatsuroaoki <tatsuro.aoki@gmail.com>
2018-07-09 19:55:50 -0700

    Form submission prints classes to page

--- script.js
+++ script.js
@@ -4,19 +4,21 @@ var courseList = [];
 
 // Assign handlers to page events.
 window.onload = function(){
-  document.querySelector("form#add-course").addEventListener("submit", addACourse)
+	document.querySelector("form#add-course").addEventListener("submit", addACourse)
 
-  document.querySelector("button#clear").addEventListener("click", clearData)
+	document.querySelector("button#clear").addEventListener("click", clearData)
 
-  document.querySelector("button#calculate").addEventListener("click", calculateAverage)
+	document.querySelector("button#calculate").addEventListener("click", calculateAverage)
 }
 
 // Form handler creates a new course object and pushes it into courseList array,
 // clears content in form fields, prints courseList objects to the page.
 // BUGFIX: This function isn't working properly- nothing gets output to the list on form submission.
-function addACourse(){
-  var grade = parseFloat(this.elements["grade"].value);
-  /*
+function addACourse(event){
+	// do not submit to server!
+	event.preventDefault()
+	var grade = parseFloat(this.elements["grade"].value);
+	/*
     TODO: validate that "grade" value is a number between 1.0 and 4.0, stop processing if it is not.
 
     Checking 'grade typeof "number"' will always return true because we called parseFloat.
@@ -25,54 +27,62 @@ function addACourse(){
     REVIEW: could we make it doubly safe by adding an HTML validation as well?
   */
 
-  // Create the new course with values from the form, push it into array of courses.
-  var newCourse = {
-    name: this.elements["name"].value,
-    grade: grade
-  }
+	// Create the new course with values from the form, push it into array of courses.
+	var newCourse = {
+		name: this.elements["name"].value,
+		grade: grade
+	}
 
-  courseList.push(newCourse)
+	courseList.push(newCourse)
 
-  clearFormFields()
-  outputList()
+	clearFormFields()
+	outputList()
 }
 
 // Calculate the average of "grade" attribute for all objects in courseList array
 // and print it in friendly message to page text.
 function calculateAverage(){
-  // Average GPA of courses equals the sum of all grades,
-  // divided by the number of course objects in courseList
-  var sum = 0
-  for(var i=0;i<courseList.length;i++){
-    sum += courseList[i].grade
-  }
-  var avg = sum / courseList.length
-  document.getElementById("result").innerHTML = "Your overall GPA is " + avg;
+	// Average GPA of courses equals the sum of all grades,
+	// divided by the number of course objects in courseList
+	var sum = 0
+	for(var i=0;i<courseList.length;i++){
+		sum += courseList[i].grade
+	}
+	var avg = sum / courseList.length
+	document.getElementById("result").innerHTML = "Your overall GPA is " + avg;
 }
 
 // Removes GPA calculation from page text.
 function clearGPA(){
-  document.getElementById("result").innerHTML = null;
+	document.getElementById("result").innerHTML = null;
 }
 
 // Clears content in form fields.
 function clearFormFields(){
-  // TODO: implement this function.
+	// TODO: implement this function.
 }
 
 // Clear out list of courses and all content shown on the page
 function clearData(){
-  courseList = []
-  clearFormFields()
-  outputList()
+	courseList = []
+	clearFormFields()
+	outputList()
 }
 
 // Prints courseList objects to the page in a readable way.
 function outputList(){
-  var list = document.getElementById("course-list");
-  /*
-    TODO: Clear the existing contents of the "list" element. Then, for each object in courseList,
+	var list = document.getElementById("course-list");
+	console.log("outputList is being called");
+	/*
+    TODO (COMPLETE): Clear the existing contents of the "list" element. Then, for each object in courseList,
     create an li element that holds the course's name and grade, and append
     it to the "list" ul element.
   */
+	list.innerHTML = "";
+	for (let i = 0; i < courseList.length; i++) {
+		//console.log(courseList[i]);
+		let item = document.createElement('li');
+		item.innerHTML = courseList[i].name + " " + courseList[i].grade;
+		list.appendChild(item);
+	}
 }
